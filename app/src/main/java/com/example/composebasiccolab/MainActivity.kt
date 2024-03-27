6583edc868be61e932b522d1fe1168a131210b62
Utkarsh Karki <utkarsh1998@icloud.com>
2024-03-27 12:54:00 -0700

    Playing around with UI

--- app/src/main/java/com/example/composebasiccolab/MainActivity.kt
+++ app/src/main/java/com/example/composebasiccolab/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.composebasiccolab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composebasiccolab.ui.theme.ComposeBasicColabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicColabTheme {
-                // A surface container using the 'background' color from the theme
+//                 A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    color = Color.LightGray,
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Greeting(name = "Hello Android!")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Greeting(name = "Welcome to Jetpack Compose!")
+                    }
                 }
             }
         }
@@ -41,6 +57,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeBasicColabTheme {
-        Greeting("Android")
+        Greeting("Androids")
     }
 }
